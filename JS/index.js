7f220fd7338386a9596196b1c48fbc0a87743176
Ragavi2909 <rrgavi03@gmail.com>
2026-05-02 23:04:21 +0530

    learned basics of js upto accessing props

---
+++ JS/index.js
@@ -0,0 +1,99 @@
+//js is all about how the website behaves whereas html is what are the contents should be on the website
+//console.log('hello world');
+
+
+//VARIABLES
+// let firstName =  'Ragavi';
+// console.log(firstName);
+/*
+rules:-
+  -variables name should not be a reserved keyword
+  -should be meaningful
+  -not to start with number
+  -should not contain space or hyphen -> use camelNotation
+  -js are case sensitive*/
+
+
+
+
+//   //CONSTANTS
+//   let interestRate = 0.5;
+//   interestRate = 60;
+//   console.log(interestRate);
+//   //Note => we can reassign let values
+
+//   const goldRate =1000;
+//   goldRate = 3000;
+//   console.log(goldRate);
+//   //Note => we cant reassign the const values and throws errormsg
+
+
+
+
+// //1.Primtive types/ Value types
+//     -Strings
+//     -Number
+//     -undefined
+//     -null
+//     -Boolean
+//   2. Reference types
+    //    -Object
+    //    -Array
+    //    -functions
+
+
+// //1.Primitive
+// let animalName = 'hamster'; //String literal
+// let number = 29;            //number literal
+// let isApproved = true;      //boolean literal
+// let lastName = undefined;
+// let selectedPost = null; //can be value so we change later
+// //console.log(animalName);
+
+
+
+
+
+
+
+//NOTE
+// js is a dynamically typed language where the type of variable can be
+// changed at runtime which is not possible in statically-typed
+// eg:  let name="ragavi";
+// let animalName = 'hamster'; //String literal
+// let number = 29;            //number literal
+// let isApproved = true;      //boolean literal
+// let lastName = undefined;
+// let selectedPost = null;
+
+
+
+
+
+
+
+
+//OBJECTS- when we deal with multiple relatable variables we put all in a single object
+// let name = 'Ragavi';
+// let age = '20';
+
+//instead write like this
+// let person = { //object literal
+//     name: 'ragav',
+//     age: 22
+// };
+// console.log(person);
+
+// //accessing props=>1.dot notation
+// person.name = 'Rajesh';
+// console.log(person.name);
+
+// //2.accessing props => bracket notation
+// person['name'] = 'kavi';
+// console.log(person.name)
+
+
+
+
+
+
