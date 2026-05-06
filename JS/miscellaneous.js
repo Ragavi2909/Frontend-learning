d18336b9496b3605d4bf7644a8d62e519583aff8
Ragavi2909 <rrgavi03@gmail.com>
2026-05-06 09:38:07 +0530

    remaining dt

---
+++ JS/miscellaneous.js
@@ -0,0 +1,35 @@
+//4.Boolean dt (used for condition checking)
+let isMale = true;
+console.log(typeof isMale); //boolean
+
+let condition = 5<3;
+console.log(condition); //prints false
+console.log(typeof condition); //boolean
+
+
+
+
+//5.null value dt
+let age = null;
+console.log(age);  //prints null
+
+
+
+
+//6.undefined dt (declared but didn't initialized)
+let DOB;
+console.log(DOB); //prints undefined
+
+
+
+
+//****JS is dynamically typed language
+let myName = "MY";
+console.log(typeof myName); //prints string
+myName = 123;
+console.log(typeof myName); //prints number
+//note -> for a single variable if we change the value, it also changes its dt (here string to number for myName)
+
+
+
+
