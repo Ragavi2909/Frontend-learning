dda7bba6bfa6fe7dbe078e2476213da9344a4219
Ragavi2909 <rrgavi03@gmail.com>
2026-05-07 23:03:11 +0530

    learned object concepts

---
+++ JS/objects.js
@@ -0,0 +1,48 @@
+// //declaring an object
+// let x;
+// console.log(x); //prints undefined
+
+
+// //declaring and initializing an object
+// let x = {};
+// console.log(typeof(x));  //prints object
+
+
+
+
+
+
+// let car = {   //here car is an object
+//     //prperties are in "key value" pair
+//     name : 'Nexon',
+//     brand : 'Tata',
+//     driveTrain : 'petrol, diesel, ev',
+//     price : 15
+// };
+// console.log(car); 
+// // //prints {
+// //   name: 'Nexon',
+// //   brand: 'Tata',
+// //   driveTrain: 'petrol, diesel, ev',
+// //   price: 15
+// // }
+// console.log(car.name); //prints (value alone)
+// delete car.price; //deletes price property
+// car['modelyear'] = 2020;
+// car['brand'] = 'honda'; //car object with brand updated to honda
+// console.log(car);  //price prop removed and prints remaining
+
+
+
+
+
+
+
+let car = {   //here car is an object
+    //prperties are in "key value" pair
+    name : 'Nexon',
+    brand : 'Tata',
+    driveTrain : 'petrol, diesel, ev',
+    price : 15
+};
+console.log(car); 
