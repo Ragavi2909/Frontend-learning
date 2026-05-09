fbef8056e33ab5f5c2030a98e2fe7f823a29a27e
Ragavi2909 <rrgavi03@gmail.com>
2026-05-09 06:38:41 +0530

    concept on objects

--- JS/objects.js
+++ JS/objects.js
@@ -1,4 +1,4 @@
-// //declaring an object
+// //declaring an object - without class we can declare an object in js
 // let x;
 // console.log(x); //prints undefined
 
@@ -38,11 +38,26 @@
 
 
 
-let car = {   //here car is an object
-    //prperties are in "key value" pair
-    name : 'Nexon',
-    brand : 'Tata',
-    driveTrain : 'petrol, diesel, ev',
-    price : 15
-};
-console.log(car); 
+// let car = {   //here car is an object
+//     //properties are in "key value" pair
+//     name : 'Nexon',
+//     brand : 'Tata',
+//     driveTrain : 'petrol, diesel, ev',
+//     price : 15,
+//     //we can use function in the object
+//     onRoadPrice(){
+//          return this.price + 2; //this means -> take this object's price
+//     }
+// };
+// console.log(car.onRoadPrice()); //prints 17
+// console.log(car); 
+
+
+
+//trivia question
+let user = {
+    name : 'Ragavi',
+}
+console.log(user['name'] = 'RagaviR');
+delete user.name;
+console.log(user);
